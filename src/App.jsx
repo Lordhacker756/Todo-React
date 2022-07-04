93bc3dd50c12165a8b1b26b1c050a5edb73379ec
Utkarsh Mishra <utkarshmishra2001@gmail.com>
2022-07-04 23:39:09 +0530

    Redesigned UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Todo from './Components/Todo'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Todo from "./Components/Todo";
 function App() {
-
-  return (
-    <Todo/>
-  )
+  return <Todo />;
 }
 
-export default App
+export default App;
